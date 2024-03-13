1e33a8f5a8315b049eef18e235d4c3abcff13959
Wenbo Li <lwb21@mails.tsinghua.edu.cn>
2024-03-13 15:55:42 +0800

    Improve lab 3 test
    - Add schematics for concurrent transaction
    
    - Remove controversial 70-write_skew_serializable.test
    
    Signed-off-by: Wenbo Li <lwb21@mails.tsinghua.edu.cn>

--- src/executors/seqscan_executor.cpp
+++ src/executors/seqscan_executor.cpp
@@ -13,6 +13,7 @@ void SeqScanExecutor::Init() {
 std::shared_ptr<Record> SeqScanExecutor::Next() {
   std::unordered_set<xid_t> active_xids;
   // 根据隔离级别，获取活跃事务的 xid（通过 context_ 获取需要的信息）
+  // 通过 context_ 获取正确的锁，加锁失败时抛出异常
   // LAB 3 BEGIN
   return scan_->GetNextRecord(context_.GetXid(), context_.GetIsolationLevel(), context_.GetCid(), active_xids);
 }
