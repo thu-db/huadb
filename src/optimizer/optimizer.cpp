5f764ef277a2c8f7ed8567fa2c637397022af8a3
Wenbo Li <lwb21@mails.tsinghua.edu.cn>
2024-05-16 02:18:32 +0800

    Add more comments in lab5
    Signed-off-by: Wenbo Li <lwb21@mails.tsinghua.edu.cn>

--- src/optimizer/optimizer.cpp
+++ src/optimizer/optimizer.cpp
@@ -43,8 +43,13 @@ std::shared_ptr<Operator> Optimizer::PushDown(std::shared_ptr<Operator> plan) {
 }
 
 std::shared_ptr<Operator> Optimizer::PushDownFilter(std::shared_ptr<Operator> plan) {
-  // 判断谓词是否为 Comparison 类型，如果是，判断是否为 ColumnValue 和 ColumnValue 的比较
-  // 若是，则该谓词为连接谓词；若不是，则该谓词为普通谓词
+  // 将 plan 转为 FilterOperator 类型
+  // 判断谓词（FilterOperator 的 predicate_ 字段）是否为 Comparison 类型，如果是，判断是否为 ColumnValue 和 ColumnValue
+  // 的比较 若是，则该谓词为连接谓词；若不是，则该谓词为普通谓词
+  // 可以将连接谓词和普通谓词存储到成员变量中，在遍历下层节点（SeqScan 和 NestedLoopJoin）时使用
+  // 遍历结束后，根据谓词是否被成功下推（可以在 PushDownSeqScan 中记录），来决定 Filter
+  // 节点是否还需在查询计划树种的原位置保留 若成功下推，则无需保留，通过 return plan->children_[0] 来删除节点
+  // 否则，直接 return plan，保留节点
   // LAB 5 BEGIN
   plan->children_[0] = PushDown(plan->children_[0]);
   return plan;
@@ -57,8 +62,9 @@ std::shared_ptr<Operator> Optimizer::PushDownProjection(std::shared_ptr<Operator
 }
 
 std::shared_ptr<Operator> Optimizer::PushDownJoin(std::shared_ptr<Operator> plan) {
-  // 判断当前查询计划树的连接谓词是否使用当前的 NestedLoopJoin 节点的列
-  // 如果有，将连接谓词添加到当前的 NestedLoopJoin 节点的 join_condition_ 中
+  // ColumnValue 的 name_ 字段为 "table_name.column_name" 的形式
+  // 判断当前查询计划树的连接谓词是否使用当前 NestedLoopJoin 节点涉及到的列
+  // 如果是，将连接谓词添加到当前的 NestedLoopJoin 节点的 join_condition_ 中
   // LAB 5 BEGIN
   for (auto &child : plan->children_) {
     child = PushDown(child);
@@ -67,14 +73,15 @@ std::shared_ptr<Operator> Optimizer::PushDownJoin(std::shared_ptr<Operator> plan
 }
 
 std::shared_ptr<Operator> Optimizer::PushDownSeqScan(std::shared_ptr<Operator> plan) {
-  // 判断当前查询计划树的普通谓词是否使用当前的 SeqScan 节点的列
-  // 如果有，在此扫描节点上方添加 Filter 节点
+  // ColumnValue 的 name_ 字段为 "table_name.column_name" 的形式
+  // 根据 table_name 与 SeqScanOperator 的 GetTableNameOrAlias 判断谓词是否匹配当前 SeqScan 节点的表
+  // 如果匹配，在此扫描节点上方添加 Filter 节点，并将其作为返回值
   // LAB 5 BEGIN
   return plan;
 }
 
 std::shared_ptr<Operator> Optimizer::ReorderJoin(std::shared_ptr<Operator> plan) {
-  // 从系统表中读取表和列的元信息
+  // 通过 catalog_.GetCardinality 和 catalog_.GetDistinct 从系统表中读取表和列的元信息
   // 可根据 join_order_algorithm_ 变量的值选择不同的连接顺序选择算法，默认为 None，表示不进行连接顺序优化
   // LAB 5 BEGIN
   return plan;
